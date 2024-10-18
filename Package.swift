b8888b704393b3e298945d3b26b338bccd86b61d
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-10-18 17:39:30 +0300

    release 2.5.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BanubaFiltersAgoraExtension",
-            url: "https://github.com/Banuba/banuba-filters-agora-extension-framework/raw/main/BanubaFiltersAgoraExtension.xcframework.zip",
-            checksum: "d66c7ecc07dd94557282df765182f154e8715230aa75f12eb9e1d0962130c6bd"
+            url: "https://github.com/Banuba/banuba-filters-agora-extension-framework/raw/refs/tags/2.5.1/BanubaFiltersAgoraExtension.xcframework.zip",
+            checksum: "a7a19933a1fcb150ecc3520a3df779fcf0d35eb8ebb2b0e300533aeaa75bf39e"
         ),
     ]
 )
